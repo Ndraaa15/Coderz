5a457374000bfd98053fafc46484259a0d490f58
Gede Indra Adi Brata <indrabrata599@gmail.com>
2024-08-23 10:00:55 +0700

    Update necesarry code

--- src/main/java/my/id/cupcakez/coderz/WelcomeMessage.java
+++ src/main/java/my/id/cupcakez/coderz/WelcomeMessage.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class WelcomeMessage {
   public String getWelcomeMessage() {
-    return "Welcome to Spring Boot!";
+    return "Welcome to Spring Boot!!!";
   }
 }
